b111e77fb8771c6f93a06bc900954cde5f67f5ab
Han Jung <hanseop.jung@gmail.com>
2026-05-31 11:55:53 +0900

    rollback

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,7 +7,6 @@ import tailwindcss from '@tailwindcss/vite';
 import pagefind from 'astro-pagefind';
 export default defineConfig({
   site: 'https://blog.eone.one',
-  trailingSlash: 'never',
   integrations: [
     mdx(),
     react(),
